c3e62252fd052fe32eb12b6006a5e9fbe2a43790
Raja <dasarirajendertect@gmail.com>
2018-09-30 13:22:42 +0530

    secondcommit

--- app/src/main/java/com/example/sam1/MainActivity.java
+++ app/src/main/java/com/example/sam1/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.sam1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview=(TextView)findViewById(R.id.textview);
+
     }
 }
